c592c511f3976fddcfb1b65986797df367c4a9b0
ogustavodias <gustavoalvesdiaz08@gmail.com>
2026-02-25 20:49:09 -0300

    refactor: lógica de transformar a entidade Product em ProductResponse movida para a própria response

--- src/main/java/com/autoflex/supply_core/dtos/ProductResponse.java
+++ src/main/java/com/autoflex/supply_core/dtos/ProductResponse.java
@@ -3,8 +3,26 @@ package com.autoflex.supply_core.dtos;
 import java.math.BigDecimal;
 import java.util.List;
 
+import com.autoflex.supply_core.models.Product;
+import com.autoflex.supply_core.models.ProductMaterial;
+
 public record ProductResponse(String name, BigDecimal price, List<MaterialAmount> materials, Boolean isProducible) {
 
+   public static ProductResponse fromEntity(Product product) {
+      List<MaterialAmount> materials = product.getMaterials().stream().map(MaterialAmount::fromEntity).toList();
+      return new ProductResponse(
+            product.getName(),
+            product.getPrice(),
+            materials,
+            materials.stream().allMatch(m -> m.stock() >= m.requiredAmount()));
+   }
+
    public record MaterialAmount(String name, Integer stock, Integer requiredAmount) {
+      public static MaterialAmount fromEntity(ProductMaterial productMaterial) {
+         return new MaterialAmount(
+               productMaterial.getMaterial().getName(),
+               productMaterial.getMaterial().getStock(),
+               productMaterial.getRequiredAmount());
+      }
    }
 }
--- src/main/java/com/autoflex/supply_core/services/ProductService.java
+++ src/main/java/com/autoflex/supply_core/services/ProductService.java
@@ -5,8 +5,6 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.autoflex.supply_core.dtos.ProductResponse;
-import com.autoflex.supply_core.dtos.ProductResponse.MaterialAmount;
-import com.autoflex.supply_core.models.Product;
 import com.autoflex.supply_core.repositories.ProductRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -18,25 +16,7 @@ public class ProductService {
    private final ProductRepository repository;
 
    public List<ProductResponse> getAllProducts() {
-      return repository.findAllByOrderByPriceDesc().stream().map(p -> {
-         List<MaterialAmount> materials = getMaterialsAmountsOfProduct(p);
-
-         return new ProductResponse(
-               p.getName(),
-               p.getPrice(),
-               materials,
-               materials.stream().allMatch(m -> m.stock() >= m.requiredAmount()));
-      }).toList();
-   }
-
-   private List<MaterialAmount> getMaterialsAmountsOfProduct(Product product) {
-      return product.getMaterials()
-            .stream()
-            .map(m -> new MaterialAmount(
-                  m.getMaterial().getName(),
-                  m.getMaterial().getStock(),
-                  m.getRequiredAmount()))
-            .toList();
+      return repository.findAllByOrderByPriceDesc().stream().map(ProductResponse::fromEntity).toList();
    }
 
 }
