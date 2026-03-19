8bd653e227277da131d2008710e080fe6d4efc1f
ogustavodias <gustavoalvesdiaz08@gmail.com>
2026-03-18 22:21:16 -0300

    feature exception handler global para captura e retorno padronizado de erros

--- src/main/java/com/autoflex/supply_core/domain/product_material/dtos/ProductMaterialCreate.java
+++ src/main/java/com/autoflex/supply_core/domain/product_material/dtos/ProductMaterialCreate.java
@@ -1,5 +1,6 @@
 package com.autoflex.supply_core.domain.product_material.dtos;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -17,5 +18,6 @@ public class ProductMaterialCreate {
 
    @NotNull(message = "amount is required.")
    @Min(value = 1, message = "The amount must be greater than or equal to one.")
+   @Max(value = -1, message = "The amount must be less than or equal to -1.")
    private Integer requiredAmount;
 }
--- src/main/java/com/autoflex/supply_core/errors/GlobalHandler.java
+++ src/main/java/com/autoflex/supply_core/errors/GlobalHandler.java
@@ -1,25 +1,78 @@
 package com.autoflex.supply_core.errors;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import lombok.extern.slf4j.Slf4j;
 
 @RestControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
 @Slf4j
 public class GlobalHandler {
 
    @ExceptionHandler(Exception.class)
    public ProblemDetail handleGlobal(Exception e) {
       String errorTrace = UUID.randomUUID().toString();
-      log.error("Internal Server Error {}: ", errorTrace, e);
+      log.error("Internal Server Error {}: {}", errorTrace, e.getMessage());
 
       ProblemDetail problem = ProblemDetail.forStatus(HttpStatus.INTERNAL_SERVER_ERROR);
       problem.setProperty("errorTrace", errorTrace);
+
+      return problem;
+   }
+
+   @ExceptionHandler(NotPermittedException.class)
+   public ProblemDetail handleNotPermitted(NotPermittedException e) {
+      String errorTrace = UUID.randomUUID().toString();
+      log.error("Not Permitted {}: {}", errorTrace, e.getMessage());
+
+      ProblemDetail problem = ProblemDetail.forStatus(HttpStatus.UNPROCESSABLE_CONTENT);
+      problem.setProperty("errorTrace", errorTrace);
+
+      return problem;
+   }
+
+   @ExceptionHandler(NotFoundException.class)
+   public ProblemDetail handleNotFound(NotFoundException e) {
+      String errorTrace = UUID.randomUUID().toString();
+      log.error("Not Found {}: {}", errorTrace, e.getMessage());
+
+      ProblemDetail problem = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
+      problem.setProperty("errorTrace", errorTrace);
+
+      return problem;
+   }
+
+   @ExceptionHandler(MethodArgumentNotValidException.class)
+   public ProblemDetail handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+      String errorTrace = UUID.randomUUID().toString();
+      log.error("Bad Request {}: {}", errorTrace,
+            e.getAllErrors().stream().map(ObjectError::getDefaultMessage).collect(Collectors.joining(", ")));
+
+      ProblemDetail problem = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+      problem.setProperty("errorTrace", errorTrace);
+      problem.setProperty("fields", e.getFieldErrors()
+            .stream().collect(
+                  Collectors.toMap(
+                        FieldError::getField,
+                        error -> List.of(error.getDefaultMessage()),
+                        (currentMsg, newMsg) -> {
+                           List<String> combined = new ArrayList<>(currentMsg);
+                           combined.addAll(newMsg);
+                           return combined;
+                        })));
 
       return problem;
    }
