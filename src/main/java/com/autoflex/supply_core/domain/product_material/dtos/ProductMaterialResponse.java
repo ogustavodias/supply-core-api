3bcad35a688401f08f79e879ac0dee6c1d560732
ogustavodias <gustavoalvesdiaz08@gmail.com>
2026-03-19 22:08:45 -0300

    chore: alteração no response de ProdutctMaterialResponse

--- src/main/java/com/autoflex/supply_core/domain/product_material/dtos/ProductMaterialResponse.java
+++ src/main/java/com/autoflex/supply_core/domain/product_material/dtos/ProductMaterialResponse.java
@@ -2,12 +2,11 @@ package com.autoflex.supply_core.domain.product_material.dtos;
 
 import com.autoflex.supply_core.domain.product_material.model.ProductMaterial;
 
-public record ProductMaterialResponse(Long id, Long materialId, String materialName, Integer stock,
+public record ProductMaterialResponse(Long id, String name, Integer stock,
       Integer requiredAmount) {
 
    public static ProductMaterialResponse fromEntity(ProductMaterial productMaterial) {
       return new ProductMaterialResponse(
-            productMaterial.getId(),
             productMaterial.getMaterial().getId(),
             productMaterial.getMaterial().getName(),
             productMaterial.getMaterial().getStock(),
